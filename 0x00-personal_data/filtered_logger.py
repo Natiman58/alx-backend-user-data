a286d14413b8a319c931bc30c9ec09411da9f2b8
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-01 19:50:40 +0300

    line reduce

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,12 +11,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: List[str],
                  message: List[str], separator) -> str:
-    """
-       returns an obfuscated message
-       :param fields: list of fields to be obfuscated
-       :message: the log message
-       :separator: the separator
-    """
+    """ returns an obfuscated message """
     for i in fields:
         message = re.sub(f'{i}=.*?{separator}',
                          f'{i}={redaction}{separator}', message)
