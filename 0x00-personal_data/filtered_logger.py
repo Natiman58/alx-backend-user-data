2b19f8df67133be3fbb255eb51536f67495ae9c4
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-01 19:51:33 +0300

    comment add

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,7 +11,12 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: List[str],
                  message: List[str], separator) -> str:
-    """ returns an obfuscated message """
+    """
+       returns an obfuscated message
+       :param fields: list of fields to be obfuscated
+       :message: the log message
+       :separator: the separator
+    """
     for i in fields:
         message = re.sub(f'{i}=.*?{separator}',
                          f'{i}={redaction}{separator}', message)
