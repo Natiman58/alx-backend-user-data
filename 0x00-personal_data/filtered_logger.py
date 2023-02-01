3c795ac0c29817e84398b03a28c3c2c0028edf82
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-01 19:43:44 +0300

    filter

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+"""
+
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str],
+                 redaction: List[str],
+                 message: List[str], separator) -> str:
+    """
+       returns an obfuscated message
+       :param fields: list of fields to be obfuscated
+       :message: the log message
+       :separator: the separator
+    """
+    for i in fields:
+        message = re.sub(f'{i}=.*?{separator}',
+                         f'{i}={redaction}{separator}', message)
+    return message
