3d271453d78ca8711bea922c09242286abd674f3
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-02 18:26:35 +0300

    check is pwd is valid

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,9 +9,20 @@ def hash_password(password: str) -> bytes:
     """
     Hashes a password
     :param password: password to hash
-    return bytes
+    return byte strings
     """
     bytes = password.encode('utf-8')  # convert pwd into byte array
     salt = bcrypt.gensalt()  # generate salt
     hash = bcrypt.hashpw(bytes, salt)  # hash password
     return hash
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks if a password is valid
+    :param hashed_password: hashed password
+    :param password: password to check
+    return boolean
+    """
+    userpw = password.encode('utf-8')  # encode user pwd into byte array
+    return bcrypt.checkpw(userpw, hashed_password)
