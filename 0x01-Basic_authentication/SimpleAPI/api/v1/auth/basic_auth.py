c8f5da6a6c7275b0bcc07b2f868949c966eb3020
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-06 23:44:53 +0300

    base64 decode

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -2,7 +2,11 @@
 """
     Definition of basic authentication
 """
+from asyncio.format_helpers import extract_stack
+import base64
+from lib2to3.pytree import convert
 from re import A
+from tkinter import E
 from api.v1.auth.auth import Auth
 
 
@@ -25,3 +29,22 @@ class BasicAuth(Auth):
         # Otherwise, return the value after Basic (after the space)
         return authorization_header.split(" ")[-1]
 
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+            returns the decoded value of base64 authorization header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            # change the base64_authorization_header to binary string
+            binary_string = base64_authorization_header.encode('utf-8')
+
+            # change the binary_string to binary code
+            binary_code = base64.b64decode(binary_string)
+
+            # return the decoded value as a string
+            return binary_code.decode('utf-8')
+        except Exception:
+            return None
