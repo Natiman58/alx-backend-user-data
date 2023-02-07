09f4a101b6532c33ac80bca2595ba7a2a3117f2f
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-07 12:49:58 +0300

    extract user email and password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -46,3 +46,20 @@ class BasicAuth(Auth):
             return binary_code.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+         self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+            return user email and password
+            from base64 decoded value
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        elif not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        elif ':' not in decoded_base64_authorization_header:
+            return None, None
+        else:
+            email = decoded_base64_authorization_header.split(':')[0]
+            password = decoded_base64_authorization_header.split(':')[1]
+            return email, password
