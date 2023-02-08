c2fd04d880ad28ba576d6274443ad9856bb94821
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-08 18:07:35 +0300

    create new session and retrieve user by using session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,10 +2,23 @@
 """
     A module that implements session authentication
 """
+from uuid import uuid4
 from .auth import Auth
 
 class SessionAuth(Auth):
     """
-
+        for session authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a session id for a user
+        using user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
