e61c7af09ffebf0acffa790f5b77c60f2d6271bd
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-08 20:46:45 +0300

    add method to get the current user using a cookie value

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,8 @@
     A module that implements session authentication
 """
 from uuid import uuid4
+
+from models.user import User
 from .auth import Auth
 
 
@@ -32,3 +34,13 @@ class SessionAuth(Auth):
             return None
         user_id = self.user_id_by_session_id.get(session_id)
         return user_id
+
+    def current_user(self, request=None):
+        """
+            returns current user instance based on a cookie value
+        """
+        cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(cookie)
+        user = User.get(user_id)
+
+        return user
