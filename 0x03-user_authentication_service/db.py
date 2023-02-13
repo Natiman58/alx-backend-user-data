1a8bb2532d52a431c42ffaadc1f8fb79c1cef895
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-13 23:10:18 +0300

    add new method to add the user to the current session

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -15,7 +15,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -28,3 +28,17 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a user to the the db"""
+        # create a new user object with the given email and pwd
+        user = User(email=email, hashed_password=hashed_password)
+
+        # add the user to the current session
+        self._session.add(user)
+
+        # commit the changes to the database
+        self._session.commit()
+
+        # return the User object the was added to the DB
+        return user
