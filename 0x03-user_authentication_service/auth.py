0dc2cbd21cdd7dfaf444db8d982189c172d33b65
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-15 12:08:53 +0300

    no need to use it outside

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,7 +75,7 @@ class Auth:
         except NoResultFound:
             return False
 
-    def _generate_uuid(self) -> Str:
+    def _generate_uuid() -> Str:
         """
             A private method to generate uuid
             and return the string format of the uuid
