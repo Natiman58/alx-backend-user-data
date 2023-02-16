d542c842e121785dd44b2e3ae73c545f70c5d387
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-16 17:44:06 +0300

    pycodestyle

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -115,7 +115,10 @@ def get_reset_password_token():
         # extract the email from the form data
         email = request.form.get('email')
         new_token = AUTH.get_reset_password_token(email)
-        response = jsonify({"email": f"{email}", "reset_token": f"{new_token}"})
+        response = jsonify({
+                            "email": f"{email}",
+                            "reset_token": f"{new_token}"
+                            })
         return response
     # if email is not found; abort(403)
     except ValueError:
